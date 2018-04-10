d908e90971be607a3e5fbe0eac73b061100d4443
lsk <alexey.dyatel@gmail.com>
2018-04-11 02:09:00 +0300

    lol

--- Views/js/developer/jqcart.js
+++ Views/js/developer/jqcart.js
@@ -17,12 +17,12 @@
       modal = '<div id="overlay"></div><div id="modal_form" class="jqcart-layout" ><div class="jqcart-checkout"></div></div>',
     orderform = '<div id="form2"><p class="jqcart-cart-title-no">Контактная информация:</p><form class="jqcart-orderform"><p><label>ФИО:</label><input type="text" name="user_name"></p><p><label>Телефон:</label><input type="text" id="user_phone" name="user_phone"></p><p><label>Адрес:</label><input type="text" name="user_address"></p><p><label>Коментарий:</label><textarea name="user_comment"></textarea></p><p><button type="submit" class="btn btn-primary btn-basket">Отправить заказ</button></p></form></div>';
   var opts = {
-		buttons: '.add_item',
-		cartLabel: 'body',
-		visibleLabel: false,
-		openByAdding: false,
+    buttons: '.add_item',
+    cartLabel: 'body',
+    visibleLabel: false,
+    openByAdding: false,
     handler: '/sendOrder',
-		currency: '$'
+    currency: '$'
   };
   var actions = {
     init: function(o) {
@@ -47,7 +47,7 @@
           }
         })
         .on('click', '.jqcart-incr', actions.changeAmount)
-				.on('input keyup', '.jqcart-amount', actions.changeAmount)
+        .on('input keyup', '.jqcart-amount', actions.changeAmount)
         .on('click', '.jqcart-del-item', actions.delFromCart)
         .on('submit', '.jqcart-orderform', actions.sendOrder)
         .on('click', '#to-form2', function(){
@@ -56,7 +56,7 @@
 
         })
         .on('reset', '.jqcart-orderform', actions.hideCart)
-				.on('click', '.jqcart-print-order', actions.printOrder);
+        .on('click', '.jqcart-print-order', actions.printOrder);
       return false;
     },
     addToCart: function(e) {
@@ -64,10 +64,10 @@
       itemData = $(this).data();
       //$("#animate").addClass("animate");
       //console.log('addClass');
-			if(typeof itemData.id === 'undefined') {
-				console.log('Отсутствует ID товара');
-				return false;
-			}
+      if(typeof itemData.id === 'undefined') {
+        console.log('Отсутствует ID товара');
+        return false;
+      }
       cartData = actions.getStorage() || {};
       if (cartData.hasOwnProperty(itemData.id)) {
         cartData[itemData.id].count++;
@@ -78,9 +78,9 @@
       actions.setStorage(cartData);
       actions.changeTotalCnt(1);
       label.show();
-			if(opts.openByAdding) {
-				actions.openCart();
-			}
+      if(opts.openByAdding) {
+        actions.openCart();
+      }
       return false;
     },
     delFromCart: function() {
@@ -97,23 +97,23 @@
     },
     changeAmount: function() {
       var $that = $(this),
-				manually = $that.hasClass('jqcart-amount'),
+        manually = $that.hasClass('jqcart-amount'),
         amount = +(manually ? $that.val() : $that.data('incr')),
         itemId = $that.closest('.jqcart-tr').data('id');
       cartData = actions.getStorage();
-			if(manually) {
-      	cartData[itemId].count = isNaN(amount) || amount < 1 ? 1 : amount;
-			} else {
-				cartData[itemId].count += amount;
-			}
+      if(manually) {
+        cartData[itemId].count = isNaN(amount) || amount < 1 ? 1 : amount;
+      } else {
+        cartData[itemId].count += amount;
+      }
       if (cartData[itemId].count < 1) {
         cartData[itemId].count = 1;
       }
-			if(manually) {
-				$that.val(cartData[itemId].count);
-			} else {
-      	$that.siblings('input').val(cartData[itemId].count);
-			}
+      if(manually) {
+        $that.val(cartData[itemId].count);
+      } else {
+        $that.siblings('input').val(cartData[itemId].count);
+      }
       actions.setStorage(cartData);
       actions.recalcSum();
       return false;
@@ -127,7 +127,7 @@
         amount = +$('.jqcart-amount', this).val();
         sum = Math.ceil((amount * $('.jqcart-price', this).text()) * 100) / 100;
         $('.jqcart-sum', this).html(sum + ' ' + opts.currency);
-				subtotal = Math.ceil((subtotal + sum) * 100) / 100;
+        subtotal = Math.ceil((subtotal + sum) * 100) / 100;
         totalCnt += amount;
       });
         
@@ -140,10 +140,10 @@
       $('.jqcart-subtotal strong').text(subtotal);
       $('.jqcart-total-cnt').text(totalCnt);
       if (totalCnt <= 0) {
-				actions.hideCart();
-				if(!opts.visibleLabel) {
-        	label.hide();
-				}
+        actions.hideCart();
+        if(!opts.visibleLabel) {
+          label.hide();
+        }
       }
       return false;
     },
@@ -154,7 +154,7 @@
     },
     openCart: function() {
       var subtotal = 0,
-			cartHtml = '';
+      cartHtml = '';
       cartData = actions.getStorage();
       orderPreview = '<div id="form1"><p class="jqcart-cart-title">Корзина </p> <div class="containerTovar">';
       var key, sum = 0, counter=1;
@@ -162,7 +162,7 @@
         if (cartData.hasOwnProperty(key)) {
             sum = Math.ceil((cartData[key].count * cartData[key].price) * 100) / 100;
             subtotal = Math.ceil((subtotal + sum) * 100) / 100;
-					
+          
             orderPreview += '<div class="jqcart-tr" data-id="' + cartData[key].id + '">';
             orderPreview += '<div class="jqcart-small-td jqcart-num">' + counter + '. </div>';
             orderPreview += '<div class="jqcart-name"> ' + cartData[key].title + '</div>';
@@ -187,7 +187,7 @@
       orderPreview += '<div class="jqcart-subtotal">Всего: <strong>' + subtotal + '</strong> ' + opts.currency + '</div>';
       orderPreview += '<button type="button" id="to-form2" class="btn btn-primary btn-basket" name="button">Оформить заказ</button></div>';
       
-			cartHtml = subtotal ? (orderPreview + orderform) : '<h2 class="jqcart-empty-cart">Корзина пуста</h2>';
+      cartHtml = subtotal ? (orderPreview + orderform) : '<h2 class="jqcart-empty-cart">Корзина пуста</h2>';
         
       $(modal).appendTo('body').find('.jqcart-checkout').html(cartHtml);
         //actions.recalcSum();
@@ -215,7 +215,7 @@
       $.ajax({
         url: opts.handler,
         type: 'POST',
-				dataType: 'json',
+        dataType: 'json',
         data: {
             orderlist: $.param(actions.getStorage()),
             userdata: $that.serialize()
@@ -251,57 +251,62 @@
         }
       });
     },
-		printOrder: function (){
-			var data = $(this).closest('.jqcart-checkout').prop('outerHTML');
-			if(!data) {
-				return false;
-			}
-			var win = window.open('', 'Печать заказа', 'width='+screen.width+',height='+screen.height),
-			cssHref = $(win.opener.document).find('link[href$="jqcart.css"]').attr('href'),
-			d = new Date(),
-			curDate = ('0' + d.getDate()).slice(-2) + '-' + ('0' + (d.getMonth() + 1)).slice(-2) + '-' + d.getFullYear() + ' ' + ('0' + d.getHours()).slice(-2) + ':' + ('0' + d.getMinutes()).slice(-2) + ':' + ('0' + d.getSeconds()).slice(-2);
-			
-			
-			win.document.write('<html><head><title>Заказ ' + curDate + '</title>');
-			win.document.write('<link rel="stylesheet" href="' + cssHref + '" type="text/css" />');
-			win.document.write('</head><body >');
-			win.document.write(data);
-			win.document.write('</body></html>');
-			
-			setTimeout(function(){
-				win.document.close(); // нужно для IE >= 10
-				win.focus(); // нужно для IE >= 10			
-				win.print();
-				win.close();
-			}, 100);
-			
-			return true;
-		},
+    printOrder: function (){
+      var data = $(this).closest('.jqcart-checkout').prop('outerHTML');
+      if(!data) {
+        return false;
+      }
+      var win = window.open('', 'Печать заказа', 'width='+screen.width+',height='+screen.height),
+      cssHref = $(win.opener.document).find('link[href$="jqcart.css"]').attr('href'),
+      d = new Date(),
+      curDate = ('0' + d.getDate()).slice(-2) + '-' + ('0' + (d.getMonth() + 1)).slice(-2) + '-' + d.getFullYear() + ' ' + ('0' + d.getHours()).slice(-2) + ':' + ('0' + d.getMinutes()).slice(-2) + ':' + ('0' + d.getSeconds()).slice(-2);
+      
+      
+      win.document.write('<html><head><title>Заказ ' + curDate + '</title>');
+      win.document.write('<link rel="stylesheet" href="' + cssHref + '" type="text/css" />');
+      win.document.write('</head><body >');
+      win.document.write(data);
+      win.document.write('</body></html>');
+      
+      setTimeout(function(){
+        win.document.close(); // нужно для IE >= 10
+        win.focus(); // нужно для IE >= 10      
+        win.print();
+        win.close();
+      }, 100);
+      
+      return true;
+    },
     setStorage: function(o) {
       localStorage.setItem('jqcart', JSON.stringify(o));
-      //localStorage.setItem('initTime', +new Date()); //Время добавления в корзину
+      localStorage.setItem('initTime', +new Date()); //Время добавления в корзину
       return false;
     },
     getStorage: function() {
-      // var limit = 24 * 3600 * 1000; // 24 часа
-      // var localStorageInitTime = localStorage.getItem('InitTime'); 
-      //   if(+new Date() - localStorageInitTime > limit) //Проверяем на актуальность корзины
-      //       localStorage.clear();
-      return JSON.parse(localStorage.getItem('jqcart'));
-    }
+        // var limit = 24 * 3600 * 1000; // 24 часа
+        // var localStorageInitTime = localStorage.getItem('InitTime');
+        // var livetime = +new Date() - localStorageInitTime;
+        // alert(livetime);
+        // alert(limit);
+        // alert(livetime-limit); 
+        // if(livetime >= limit) {
+        //      localStorage.clear(); //Проверяем на актуальность корзины
+        //  }
+        return JSON.parse(localStorage.getItem('jqcart'));
+    },
   };
   var methods = {
-		clearCart: function(){
-			localStorage.removeItem('jqcart');
-			label[opts.visibleLabel ? 'show' : 'hide']().find('.jqcart-total-cnt').text(0);
-			actions.hideCart();
-		},
-		openCart: actions.openCart,
-		printOrder: actions.printOrder,
-		test: function(){
-			actions.getStorage();
-		}
-	};
+    clearCart: function(){
+      localStorage.removeItem('jqcart');
+      label[opts.visibleLabel ? 'show' : 'hide']().find('.jqcart-total-cnt').text(0);
+      actions.hideCart();
+    },
+    openCart: actions.openCart,
+    printOrder: actions.printOrder,
+    test: function(){
+      actions.getStorage();
+    }
+  };
   $.jqCart = function(opts) {
     if (methods[opts]) {
       return methods[opts].apply(this, Array.prototype.slice.call(arguments, 1));
@@ -312,4 +317,3 @@
     }
   };
 })(jQuery);
-
